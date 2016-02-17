8f9daad5b41fe0bb60f27cd4d736aed64fb02e71
Mihail Kaktyc <misha.andryushen@gmail.com>
2016-02-17 08:11:39 +0700

    all file

---
+++ index.php
@@ -0,0 +1,288 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>footbol</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:700,400,300' rel='stylesheet' type='text/css'>
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- build:css styles/vendor.css -->
+    <!-- bower:css -->
+    <!-- endbower -->
+    <!-- endbuild -->
+
+    <!-- build:css styles/main.css -->
+    <link rel="stylesheet" href="styles/main.css">
+    <!-- endbuild -->
+
+    <!-- build:js scripts/vendor/modernizr.js -->
+    <script src="/bower_components/modernizr/modernizr.js"></script>
+    <!-- endbuild -->
+  </head>
+  <body>
+    <!--[if lt IE 10]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <div class="container">
+      <div class="pop-up">
+        <div class="gold-block">
+          <div class="gold-block__info-top">
+            <div class="gold-block__info-top_left">
+              <p class="gold-block__info-top_name">Lionel Messi</p>
+              <p class="gold-block__info-top_status">Золотой</p>
+            </div>
+            <div class="gold-block__info-top_right">
+              <p class="gold-block__info-global_number">94</p>
+              <p class="gold-block__info-global_assoc">ПФА</p>
+            </div>
+          </div>
+          <div class="gold-block__info-global">
+            <div class="gold-block__info-global_player">
+              <div class="icon-left">
+                <img src="images/team-logo.png" height="40" width="40" alt="">
+                <img class="icon-left_location" src="images/location.png" height="25" width="40" alt="">
+              </div>
+              <img src="images/player.png" height="115" width="109" alt="">
+              <div class="player-right">
+                <p class="gold-block__info-top_name">10</p>
+                <p class="gold-block__info-top_status">КОНТ</p>
+              </div>
+            </div>
+            <div class="gold-block__info-global__characteristics characteristics_center">
+              <p><b>92</b> СКР</p> <p><b>88</b> УД</p>
+              <p><b>86</b> ПРД </p> <p><b>95</b> ДРБ</p>
+              <p><b>24</b> ЗАЩ</p> <p><b>62</b> ФИЗ</p>
+            </div>
+          </div>
+          <div class="gold-block__info-global__button">
+            <a href="#" class="button">Продать игрока</a>
+            <a href="#" class="button">Продлить контракт</a>
+            <a href="#" class="button">Перевести в резерв</a>
+          </div>
+        </div>
+        <div class="feature">
+          <ul class="feature__menu">
+            <li>Характеристики игрока</li>
+            <li>Информация</li>
+            <li>Продлить контракт</li>
+            <li>Продать игрока</li>
+          </ul>
+          <div class="container-info">
+            <table class="table-count">
+              <colgroup>
+              <col style="width: 122px">
+              <col style="width: 44px">
+              </colgroup>
+                <tr>
+                  <th>Темп</th>
+                  <th>92</th>
+                </tr>
+                <tr>
+                  <td>Разгон</td>
+                  <td>95</td>
+                </tr>
+                <tr>
+                  <td>Скор.Рывок</td>
+                  <td>90</td>
+                </tr>
+              </table>
+
+              <table class="table-count">
+              <colgroup>
+              <col style="width: 122px">
+              <col style="width: 44px">
+              </colgroup>
+                <tr>
+                  <th>Защита</th>
+                  <th>24</th>
+                </tr>
+                <tr>
+                  <td>Перехваты</td>
+                  <td>22</td>
+                </tr>
+                <tr>
+                  <td>Точность игр. гол.</td>
+                  <td>71</td>
+                </tr>
+                <tr>
+                  <td>Опека</td>
+                  <td>13</td>
+                </tr>
+                <tr>
+                  <td>Обычный подбор</td>
+                  <td>23</td>
+                </tr>
+                <tr>
+                  <td>Подкат</td>
+                  <td>21</td>
+                </tr>
+              </table>
+              <table class="table-count">
+              <colgroup>
+              <col style="width: 122px">
+              <col style="width: 44px">
+              </colgroup>
+                <tr>
+                  <th>Физич. характера</th>
+                  <th>62</th>
+                </tr>
+                <tr>
+                  <td>Прыжки</td>
+                  <td>95</td>
+                </tr>
+                <tr>
+                  <td>Выносливость</td>
+                  <td>90</td>
+                </tr>
+                <tr>
+                  <td>Сила</td>
+                  <td>95</td>
+                </tr>
+                <tr>
+                  <td>Напор</td>
+                  <td>90</td>
+                </tr>
+                           </table>
+              <table class="table-count">
+              <colgroup>
+              <col style="width: 122px">
+              <col style="width: 44px">
+              </colgroup>
+                <tr>
+                  <th>Дриблинг</th>
+                  <th>95</th>
+                </tr>
+                <tr>
+                  <td>Ловкость</td>
+                  <td>92</td>
+                </tr>
+                <tr>
+                  <td>Баланс</td>
+                  <td>95</td>
+                </tr>
+                <tr>
+                  <td>Реакция</td>
+                  <td>92</td>
+                </tr>
+                <tr>
+                  <td>Владения мячом</td>
+                  <td>96</td>
+                </tr>
+                <tr>
+                  <td>Дриблинг</td>
+                  <td>96</td>
+                </tr>
+              </table>
+              <table class="table-count">
+              <colgroup>
+              <col style="width: 122px">
+              <col style="width: 44px">
+              </colgroup>
+                <tr>
+                  <th>Пасы</th>
+                  <th>86</th>
+                </tr>
+                <tr>
+                  <td>Видение игры</td>
+                  <td>90</td>
+                </tr>
+                <tr>
+                  <td>Навесы</td>
+                  <td>80</td>
+                </tr>
+                <tr>
+                  <td>Точность своб.ударов</td>
+                  <td>90</td>
+                </tr>
+                <tr>
+                  <td>Короткие пасы</td>
+                  <td>88</td>
+                </tr>
+                <tr>
+                  <td>Длинные пасы</td>
+                  <td>79</td>
+                </tr>
+                <tr>
+                  <td>Подкрутка</td>
+                  <td>89</td>
+                </tr>
+              </table>
+              <table class="table-count">
+              <colgroup>
+              <col style="width: 122px">
+              <col style="width: 44px">
+              </colgroup>
+                <tr>
+                  <th>Удары</th>
+                  <th>88</th>
+                </tr>
+                <tr>
+                  <td>Выбор позиции</td>
+                  <td>55</td>
+                </tr>
+                <tr>
+                  <td>Точность удара</td>
+                  <td>36</td>
+                </tr>
+                <tr>
+                  <td>Сила удара</td>
+                  <td>2</td>
+                </tr>
+                <tr>
+                  <td>Дальние удары</td>
+                  <td>63</td>
+                </tr>
+                <tr>
+                  <td>Удары с лета</td>
+                  <td>23</td>
+                </tr>
+                <tr>
+                  <td>Пенальти</td>
+                  <td>63</td>
+                </tr>
+              </table>
+            </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      e.src='https://www.google-analytics.com/analytics.js';
+      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+      ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
+
+    <!-- build:js scripts/vendor.js -->
+    <!-- bower:js -->
+    <script src="/bower_components/jquery/dist/jquery.js"></script>
+    <!-- endbower -->
+    <!-- endbuild -->
+
+    <!-- build:js scripts/plugins.js -->
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/affix.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/alert.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/tooltip.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/modal.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/transition.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/button.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/popover.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/carousel.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/scrollspy.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/collapse.js"></script>
+    <script src="/bower_components/bootstrap-sass/assets/javascripts/bootstrap/tab.js"></script>
+    <!-- endbuild -->
+
+    <!-- build:js scripts/main.js -->
+    <script src="scripts/main.js"></script>
+    <!-- endbuild -->
+  </body>
+</html>
